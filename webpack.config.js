705ea27e269e2a90f16e5c67a331de0e675b9c4d
ebisLab <ebaiden@techie.com>
2021-05-06 01:15:29 -0400

    css loader and style loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,54 @@
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
-module.exports={
-    entry:"./index.js",
-    output:{
-        path: __dirname + "/dist",
-        filename:"./bundle.js"
-    },
-    module:{
-        rules:[
-            {test: /\.(js|jsx)$/, loader:'babel-loader', exclude: /node_modules/}
-        ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({template: './index.html'})
-    ]
-}
+module.exports = {
+  entry:"./index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+          test:/\.css$/,
+          use:['style-loader', 'css-loader']
+      }
+    ],
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  mode:'development',
+  plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new HtmlWebpackPlugin({template: './index.html'})
+    ],
+  devServer: {
+    contentBase: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
+};
+
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports={
+//     entry:"./index.js",
+//     output:{
+//         path: __dirname + "/dist",
+//         filename:"./bundle.js"
+//     },
+//     module:{
+//         rules:[
+//             {test: /\.(js|jsx)$/, loader:'babel-loader', exclude: /node_modules/}
+//         ]
+//     },
+//     plugins:[
+//         new HtmlWebpackPlugin({template: './index.html'})
+//     ]
+// }
